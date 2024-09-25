6deec9cfe95d83007791685448b8bbee1b2a7b2d
Luis Henrique <luissecundes@gmail.com>
2024-09-25 17:02:42 -0300

    First commit of the project

---
+++ dist/classes.js
@@ -0,0 +1,23 @@
+"use strict";
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        // this.validate();
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+// let user = new User('Carlos', 'Ferreira', 30);
+// console.log(user.getFullName());
+class Admin extends User {
+    getFullName() {
+        return `Dr. ${this.firstName} ${this.lastName}`;
+    }
+}
+let admin = new Admin('Carlos', 'Ferreira', 30);
+console.log(admin.getFullName());
+
+// Alteração para subir para o git
